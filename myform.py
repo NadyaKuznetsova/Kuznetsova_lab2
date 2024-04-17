7e2c3dd804913b1fac71d1ac16759e59c0573acf
Nadya <kuznechik25092005@gmail.com>
2024-04-17 11:13:09 +0300

    Добавление комментариев

--- myform.py
+++ myform.py
@@ -4,17 +4,21 @@ import re
 import datetime
 import json
 
+#Пустой словарь
 question = {}
 
+#Функция для проверки вопроса
 def check_question_validity(quest):
     if len(quest) > 3 and not quest.isdigit():
         return True
     return False
 
+#Функция для обновления файла json
 def update_json_file(data):
     with open('data.json', 'w') as file:
         json.dump(data, file, indent=4)
 
+#Функция для загрузки данных из файла json
 def load_json_file():
     try:
         with open('data.json', 'r') as file:
@@ -23,24 +27,30 @@ def load_json_file():
         return {}
 
 @post('/home')
+#Функция для обработки полей формы
 def my_form():
     quest = request.forms.get('QUEST')
     mail = request.forms.get('ADRESS')
     username = request.forms.get('USERNAME')
 
+    #Проверка, что поля не пустые
     if not quest or not mail or not username:
         return "Please fill in all fields of the form"
 
-    if not re.match(r"[\w\.\\\-/?%]{2,30}@[a-zA-Z\.-]{1,9}.[a-zA-Z]{2,7}", mail):
+    #Паттерн для обработки email
+    if not re.match(r"[\w\.\-?%]{2,30}@[a-zA-Z]{1,9}(\.[a-zA-Z]{2,7}(\.[a-zA-Z]{2,7}))?", mail):
         return "Invalid email address format"
 
+    #Получение сегодняшней даты
     if not check_question_validity(quest):
         return "Invalid question format"
 
     current_date = datetime.datetime.now().strftime("%Y-%m-%d")
 
+    #Загрузка данных из файла json
     data = load_json_file()
 
+    #Обновление данных в файле json в зависимости от наличия email в словаре data
     if mail in data:
         if quest not in data[mail]:
             data[mail].append(quest)
